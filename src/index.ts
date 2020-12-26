40719c9d8ace98e05661505d7cb774b41706c96f
A Faruk Gonullu <af@gonul.lu>
2020-12-26 15:06:39 +0300

    FSO: end of exercise 9.11

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express"
+import diagnoseRouter from "./routes/diagnoses"
+import patientRouter from "./routes/patients"
 const app = express()
 app.use(express.json())
 
@@ -17,6 +19,9 @@ app.get("/ping", (_req, res) => {
   console.log("someone pinged here")
   res.json({ response: "pong" })
 })
+
+app.use("/api/diagnoses", diagnoseRouter)
+app.use("/api/patients", patientRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
---
+++ src/services/patientService.ts
@@ -0,0 +1,22 @@
+import patientData from "../../data/patients"
+
+import { NonSensitivePatient } from "../types"
+
+const getPatients = (): NonSensitivePatient[] => {
+  return patientData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }))
+}
+
+const addPatient = () => {
+  return null
+}
+
+export default {
+  getPatients,
+  addPatient,
+}
